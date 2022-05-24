a250de253a0e0012a63976545f6400c6b296f3b2
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-05-25 00:47:41 +0600

    order api delete done

--- index.js
+++ index.js
@@ -78,6 +78,19 @@ async function run() {
             res.send(result);
         });
 
+        app.get("/orders", async (req, res) => {
+            const orders = await orderCollection.find().toArray();
+            res.send(orders);
+        });
+
+        //---order delete
+        app.delete("/orders/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        });
+
         //add review dashboard post user review
         app.post('/myReview', async (req, res) => {
             const review = req.body;
