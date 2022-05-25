6a908920169f8e05dfd906927ec34456b01dbc05
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-05-25 08:39:24 +0600

    make admin when a user will be admin added

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ async function run() {
             res.send(users)
         })
 
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
         //--- users-start----
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
@@ -72,12 +79,21 @@ async function run() {
         //for admin---
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email }
-            const updatedDoc = {
-                $set: { role: 'admin' },
-            };
-            const result = await userCollection.updateOne(filter, updatedDoc);
-            res.send(result);
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester })
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email }
+                const updatedDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updatedDoc);
+                res.send(result);
+            }
+
+            else {
+                res.status(403).send({ message: 'forbidden' })
+            }
+
         })
 
 
